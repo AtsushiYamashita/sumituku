63312326459528933188079f635c7bb8e4bc5c16
Atsushi.y <yays505@gmail.com>
2018-01-25 12:51:57 +0900

    Update main page design and setting data in controll

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,5 +1,12 @@
 class InquiryController < ApplicationController
   def main
+	@data = Inquiry.new
+	@summary = ['== お問い合わせ・ご意見の種類 ==',
+	'商品の発注について',
+	'商品の返品について',
+	'サイトのデザインについて',
+	'規約違反ユーザーの報告',
+	'その他のご意見']
   end
 
   def confirmation
